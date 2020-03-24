ab2114b4ae66b3c0555071ef731ded65de612a69
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-03-24 20:19:19 +0700

    Format code in Meal Navigator

--- navigation/MealNavigator.js
+++ navigation/MealNavigator.js
@@ -1,30 +1,31 @@
-import * as React from "react";
-import { Platform } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import CategoriesScreen from "../screens/MealApp/CategoriesScreen";
-import CategoryMealsScreen from "../screens/MealApp/CategoryMealsScreen";
-import FavoritesScreen from "../screens/MealApp/FavoritesScreen";
-import FilterScreen from "../screens/MealApp/FilterScreen";
-import MealDetailScreen from "../screens/MealApp/MealDetailScreen";
+import CategoriesScreen from '../screens/MealApp/CategoriesScreen';
+import CategoryMealsScreen from '../screens/MealApp/CategoryMealsScreen';
+import FavoritesScreen from '../screens/MealApp/FavoritesScreen';
+import FilterScreen from '../screens/MealApp/FilterScreen';
+import MealDetailScreen from '../screens/MealApp/MealDetailScreen';
 
 const Stack = createStackNavigator();
 
 const MealNavigator = (
   <NavigationContainer>
     <Stack.Navigator
+      mode="card"
       screenOptions={{
         headerStyle: {
-          backgroundColor: Platform.OS === "android" ? "orange" : ""
+          backgroundColor: Platform.OS === 'android' ? 'orange' : '',
         },
-        headerTintColor: Platform.OS === "android" ? "white" : "orange"
+        headerTintColor: Platform.OS === 'android' ? 'white' : 'orange',
       }}
     >
       <Stack.Screen
         name="Categories"
         component={CategoriesScreen}
-        options={{ title: "Meal Categories" }}
+        options={{ title: 'Meal Categories' }}
       />
       <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
       <Stack.Screen name="Favorites" component={FavoritesScreen} />
