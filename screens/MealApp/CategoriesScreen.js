6ddec1e4475fa6889645fead7401b710e2774ef9
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-03-24 20:19:33 +0700

    Refactor code in CategoriesScreen

--- screens/MealApp/CategoriesScreen.js
+++ screens/MealApp/CategoriesScreen.js
@@ -1,38 +1,33 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
-import VTTExt from '../../components/Text/VTTExt';
+import { FlatList } from 'react-native-gesture-handler';
 
 import { CATEGORIES } from '../../data/dummy-data';
+import CategoryGridItem from '../../components/CategoryGridItem';
 
 const CategoriesScreen = ({ navigation }) => {
-  const renderGridItem = (itemData) => (
-    <View style={[styles.gridItem, { backgroundColor: itemData.item.color }]}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('CategoryMeals', {
-            categoryId: itemData.item.id,
-          });
-        }}
-      >
-        <View>
-          <VTTExt
-            customStyle={{ textAlign: 'right', padding: 10, color: 'white' }}
-          >
-            {itemData.item.title}
-          </VTTExt>
-        </View>
-      </TouchableOpacity>
-    </View>
+  const handlePressCategory = (id) => {
+    navigation.navigate('CategoryMeals', {
+      categoryId: id,
+    });
+  };
+
+  const renderGridItem = ({ item }) => (
+    <CategoryGridItem
+      item={item}
+      onPress={() => handlePressCategory(item.id)}
+    />
   );
 
   return (
-    <FlatList
-      keyExtractor={(item) => item.id}
-      numColumns={2}
-      data={CATEGORIES}
-      renderItem={renderGridItem}
-    />
+    <View style={styles.screen}>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        data={CATEGORIES}
+        renderItem={renderGridItem}
+      />
+    </View>
   );
 };
 
@@ -41,21 +36,5 @@ export default CategoriesScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-  },
-  gridItem: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    borderRadius: 10,
-    flexGrow: 1,
-    margin: 15,
-    height: 150,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
 });
