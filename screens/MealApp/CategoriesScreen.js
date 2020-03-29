7be1a01d9ad2af12d98d28f8e7abf0223ed89afb
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-03-29 09:56:59 +0700

    Styling the categories screen

--- screens/MealApp/CategoriesScreen.js
+++ screens/MealApp/CategoriesScreen.js
@@ -36,5 +36,6 @@ export default CategoriesScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    backgroundColor: 'white'
   },
 });
--- screens/MealApp/CategoryMealsScreen.js
+++ screens/MealApp/CategoryMealsScreen.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CustomButton from '../../components/CustomButton';
 
-import { CATEGORIES } from '../../data/dummy-data';
+import { CATEGORIES, MEALS } from '../../data/dummy-data';
+import VTTExt from '../../components/Text/VTTExt';
+import { FlatList } from 'react-native-gesture-handler';
+import MealItem from '../../components/MealItem';
+import MealList from '../../components/MealList';
 
 const CategoryMealScreen = ({ navigation, route }) => {
   const { categoryId } = route.params;
-  const selectedCategory = CATEGORIES.find((c) => c.id === categoryId);
+  const displayMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(categoryId) >= 0
+  );
+
+  const _renderMealItem = ({ item }) => (
+    <MealItem item={item} onSelectMeal={() => handleSelectMeal(item.id)} />
+  );
+
+  const handleSelectMeal = (id) => {
+    navigation.navigate('MealDetail', {
+      mealId: id,
+    });
+  };
 
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <CustomButton onPress={() => navigation.goBack()}>Go back</CustomButton>
+      <MealList data={displayMeals} renderItem={_renderMealItem} />
     </View>
   );
 };
