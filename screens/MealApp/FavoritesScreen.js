f30efb75e8dbc320be88a20bf64e753119cd7706
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-03-29 09:57:23 +0700

    Display the favorite meal in FavortieScreen

--- screens/MealApp/FavoritesScreen.js
+++ screens/MealApp/FavoritesScreen.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import CustomButton from "../../components/CustomButton";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MealList from '../../components/MealList';
+import MealItem from '../../components/MealItem';
+import { MEALS } from '../../data/dummy-data';
 
 const FavoriteScreen = ({ navigation }) => {
+  const displayMeals = MEALS.filter((meal) => ['m1', 'm2'].includes(meal.id));
+
+  const handleSelectMeal = (id) => {
+    navigation.navigate('MealDetail', {
+      mealId: id,
+    });
+  };
+
+  const _renderMealItem = ({ item }) => (
+    <MealItem item={item} onSelectMeal={() => handleSelectMeal(item.id)} />
+  );
   return (
     <View style={styles.screen}>
-      <Text>The Favorite Screen</Text>
-      <CustomButton onPress={() => navigation.navigate("Categories")}>
-        Go to categories
-      </CustomButton>
+      <MealList data={displayMeals} renderItem={_renderMealItem} />
     </View>
   );
 };
@@ -18,7 +28,7 @@ export default FavoriteScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
