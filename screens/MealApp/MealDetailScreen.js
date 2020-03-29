23dfe327f98555f0caf85efccc8120e9aa3c7ea5
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-03-29 09:56:40 +0700

    Set titles when render meal detail screen

--- screens/MealApp/MealDetailScreen.js
+++ screens/MealApp/MealDetailScreen.js
@@ -1,10 +1,31 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import { StyleSheet, Text, View } from 'react-native';
+import { MEALS } from '../../data/dummy-data';
 
-const MealDetailScreen = () => {
+const MealDetailScreen = ({ navigation, route }) => {
+  const { mealId } = route.params;
+
+  const mealDetail = MEALS.find((m) => m.id === mealId);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: mealDetail.title,
+      headerRight: () => (
+        <AntDesign
+          size={25}
+          name="hearto"
+          onPress={() => {
+            console.log('Clicked');
+          }}
+        />
+      ),
+    });
+  }, [navigation]);
   return (
     <View style={styles.screen}>
       <Text>The Meal Detail Screen</Text>
+      <Text>{mealId}</Text>
     </View>
   );
 };
@@ -14,7 +35,7 @@ export default MealDetailScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
